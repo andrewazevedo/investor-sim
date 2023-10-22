8c605b03c2031dd712b6ffd3cba7a51c35ad86b0
Andrew <azevedoa66@gmail.com>
2023-10-22 19:41:54 -0400

    Fixed the saved games not saving

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-let saveFile = 1;
+let saveFile = 0;
+let selectedStock = 0;
+
+let stocks = {
+    stock1: {name: "Stock #1"},
+    stock2: {name: "Stock #2"},
+    stock3: {name: "Stock #3"}
+}
+
+let defaultFile = {
+    balance: 10000,
+    salary: 1000,
+    month: 0,
+    year: 2023,
+    age: 18, 
+    stocks: []
+}
 
 let file = {
     balance: 10000,
@@ -10,68 +26,39 @@ let file = {
     stocks: []
 };
 
-let file1 = {
-    balance: 10000,
-    salary: 1000,
-    month: 0,
-    year: 2023,
-    age: 18, 
-    stocks: []
-}
-
-let file2 = {
-    balance: 10000,
-    salary: 1000,
-    month: 0,
-    year: 2023,
-    age: 18,
-    stocks: []
-}
-
-let file3 = {
-    balance: 10000,
-    salary: 1000,
-    month: 0,
-    year: 2023,
-    age: 18,
-    stocks: []
-}
-
 function setSaveFile(save) {
-    saveFile = save;
-    saveGame();
+    if (localStorage.length === 0) {
+        localStorage.setItem("file1", JSON.stringify(defaultFile));
+        localStorage.setItem("file2", JSON.stringify(defaultFile));
+        localStorage.setItem("file3", JSON.stringify(defaultFile));
+        localStorage.setItem("saveFile", save);
+    } else {
+        localStorage.setItem("saveFile", save);
+    }
+    
 }
 
 function saveGame() {
+    saveFile = Number(localStorage.getItem("saveFile"));
+    localStorage.setItem("selectedStock", selectedStock.toString());
     if (saveFile === 1) {
-        localStorage.setItem("saveFile", saveFile.toString());
         localStorage.setItem("file1", JSON.stringify(file));
-        localStorage.setItem("file2", JSON.stringify(file2));
-        localStorage.setItem("file3", JSON.stringify(file3));
     } else if (saveFile === 2) {
-        localStorage.setItem("saveFile", saveFile.toString());
-        localStorage.setItem("file1", JSON.stringify(file1));
         localStorage.setItem("file2", JSON.stringify(file));
-        localStorage.setItem("file3", JSON.stringify(file3));
     } else if (saveFile === 3) {
-        localStorage.setItem("saveFile", saveFile.toString());
-        localStorage.setItem("file1", JSON.stringify(file1));
-        localStorage.setItem("file2", JSON.stringify(file2));
         localStorage.setItem("file3", JSON.stringify(file));
-    }
+    }   
 }
 
 function loadGame() {
     saveFile = Number(localStorage.getItem("saveFile"));
-    file1 = JSON.parse(localStorage.getItem("file1"));
-    file2 = JSON.parse(localStorage.getItem("file2"));
-    file3 = JSON.parse(localStorage.getItem("file3"));
+    selectedStock = Number(localStorage.getItem("selectedStock"));
     if (saveFile === 1) {
-        file = file1;
+        file = JSON.parse(localStorage.getItem("file1"));
     } else if (saveFile === 2) {
-        file = file2;
+        file = JSON.parse(localStorage.getItem("file2"));
     } else if (saveFile === 3) {
-        file = file3;
+        file = JSON.parse(localStorage.getItem("file3"));
     }
     updateScreen();
 }
@@ -79,35 +66,16 @@ function loadGame() {
 function deleteSaveFile() {
     save = Number(localStorage.getItem("saveFile"));
     if (save === 1) {
-        file1.balance = 10000;
-        file1.salary = 1000;
-        file1.month = 0;
-        file1.year = 2023;
-        file1.age = 18;
-        file1.stocks = [];
-        saveGame();
+        localStorage.setItem("file1", JSON.stringify(defaultFile));
     } else if (save === 2) {
-        file2 = {
-            balance: 10000,
-            salary: 1000,
-            month: 0,
-            year: 2023,
-            age: 18
-        }
-        saveGame();
+        localStorage.setItem("file2", JSON.stringify(defaultFile));
     } else if (save === 3) {
-        file3 = {
-            balance: 10000,
-            salary: 1000,
-            month: 0,
-            year: 2023,
-            age: 18
-        }
-        saveGame();
+        localStorage.setItem("file3", JSON.stringify(defaultFile));
     }
 }
 
 function simulate() {
+    loadGame();
     file.balance += file.salary;
     if (file.month < 11) {
         file.month += 1;
@@ -115,9 +83,9 @@ function simulate() {
         file.month = 0;
         file.year += 1;
         file.age += 1;
-    }
+    } 
     saveGame();
-    updateScreen();
+    updateScreen();   
 }
 
 function updateScreen() {
@@ -145,6 +113,23 @@ function updateScreen() {
             elements[i].innerHTML = "Monthly Salary: $" + file.salary;
         }
     }
+    if (document.getElementsByClassName("stock-title")) {
+        let elements = document.getElementsByClassName("stock-title");
+        for (var i = 0; i < elements.length; i++) {
+            if (selectedStock === 1) {
+                elements[i].innerHTML = stocks.stock1.name;
+            } else if (selectedStock === 2) {
+                elements[i].innerHTML = stocks.stock2.name;
+            } else if (selectedStock === 3) {
+                elements[i].innerHTML = stocks.stock3.name;
+            }
+             
+        }
+    }
+}
+
+function selectStock(stock) {
+    selectedStock = stock;
 }
 
 loadGame();
